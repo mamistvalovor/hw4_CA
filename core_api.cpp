fa27afd8ce51b7f639877f25a4c84b6e7df4705f
Or Mamistvalov <mamistvalovor@campus.technion.ac.il>
2021-01-11 21:38:45 +0200

    created solution

--- core_api.cpp
+++ core_api.cpp
@@ -3,20 +3,45 @@
 #include "core_api.h"
 #include "sim_api.h"
 
+#include <vector>
 #include <stdio.h>
 
+using namespace std;
+
+
+class inst_properties {
+public:
+	int count_ = 0;
+	Instruction inst_;
+};
+
+class multithread {
+public:
+
+	int thread_num_ = 0;
+
+	vector< vector< inst_properties > > vec_threads_;
+	vector<tcontext*> vec_regs_;
+
+
+
+
+};
 
 void CORE_BlockedMT() {
+
+
+
 }
 
 void CORE_FinegrainedMT() {
 }
 
-double CORE_BlockedMT_CPI(){
+double CORE_BlockedMT_CPI() {
 	return 0;
 }
 
-double CORE_FinegrainedMT_CPI(){
+double CORE_FinegrainedMT_CPI() {
 	return 0;
 }
 
